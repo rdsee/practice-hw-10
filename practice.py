ed20c870bc4670e7648b61488d45b2d31e9e71b7
rdsee <rost.slobod@gmail.com>
2026-04-22 16:26:21 +0300

    import math examples

--- practice.py
+++ practice.py
@@ -1,17 +1,26 @@
-#Modules
-import random
-# from random import *
-# from random import randint, choice
+# #Modules
+# import random
+# # from random import *
+# # from random import randint, choice
+#
+# print(random.randint(1,100)) #random numbers
+#
+# print(random.choice("qwerty")) # random symbols
+#
+# print(random.randrange(10)) #random nums in range
+# print(random.randrange(1, 30))
+# print(random.randrange(1, 30, 2))
+#
+# nums = [1, 2, 3, 4, 5]
+# random.shuffle(nums)# random position in list
+# print(nums)
 
-print(random.randint(1,100)) #random numbers
-
-print(random.choice("qwerty")) # random symbols
-
-print(random.randrange(10)) #random nums in range
-print(random.randrange(1, 30))
-print(random.randrange(1, 30, 2))
-
-nums = [1, 2, 3, 4, 5]
-random.shuffle(nums)# random position in list
-print(nums)
-
+# import math
+#
+# print(-math.inf) # -infinity
+# print(math.inf) # +infinity
+# print(math.ceil(10.2)) #огругление в большую сторону
+# print(math.floor(10.999)) #огругление в меньшую сторону
+# print(math.factorial(10)) #функция факториала
+# print(math.pow(2 , 4)) #перевод в степень
+# print(math.sqrt(9)) #квадратный корень
